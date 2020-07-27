9e2bb2171ec07a5dd92408cacbd89f3b645ad470
Badhon100 <badhonx102@gmail.com>
2020-07-27 18:50:31 +0600

    created todo-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { Todo } from './models/todo.model';
 
 @NgModule({
   declarations: [
+    
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
